4fb426dabc8806bb9f6ca9539ac8271685245250
Fernando Morales <fermorales006@hotmail.com>
2021-02-05 11:57:45 -0300

    WTF

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,7 +22,6 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
-import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 @NgModule({
   declarations: [
@@ -37,8 +36,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     TestErrorsComponent,
     NotFoundComponent,
     ServerErrorComponent,
-    MemberCardComponent,
-    MemberEditComponent
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
