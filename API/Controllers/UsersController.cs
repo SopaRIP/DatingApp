db0ba0f1eed590706535e5282896a040e86acf0d
Fernando Morales <fermorales006@hotmail.com>
2021-02-08 09:44:16 -0300

    Section 10 Class 119

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -50,7 +50,7 @@ namespace API.Controllers
 
             if (await _userRepository.SaveAllAsync()) return NoContent();
 
-            return BadRequest("Failed to update user")
+            return BadRequest("Failed to update user");
         }
     }
 }
