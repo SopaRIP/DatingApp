149d47a1410d117dd223adc77a103dce3aec0059
Fernando Morales <fermorales006@hotmail.com>
2021-02-11 16:30:46 -0300

    Section 16 Class 204. Messages Table error

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -31,12 +31,8 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-
             //New User
             user.UserName = registerDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
 
             _Context.Users.Add(user);
             await _Context.SaveChangesAsync();
@@ -59,15 +55,6 @@ namespace API.Controllers
                 .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("Invalid username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-            }
 
             return new UserDto
             {
