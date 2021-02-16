5c3cc92e7b606ff759d31bf78c52c28d2694da29
Fernando Morales <fermorales006@hotmail.com>
2021-02-16 09:46:01 -0300

    Test4 heroku fianl

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,7 +36,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAplicationServices(_config);
+            services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddCors();
             services.AddIdentityServices(_config);
